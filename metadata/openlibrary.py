937df5d2acfa7d2bd37d60392ff35556c97cf129
Juraj Borza <jborza@gmail.com>
2025-05-04 13:59:36 +0200

    cleaned up openlibrary search

--- metadata/openlibrary.py
+++ metadata/openlibrary.py
@@ -41,7 +41,6 @@ def get_book_data(title):
     if "error" in book_data:
         return book_data
 
-    # TODO obtain cover image from cover_i
     # TODO use blurha.sh to generate a placeholder image
     
     # Extract relevant fields
@@ -50,18 +49,13 @@ def get_book_data(title):
         "author_name": book_data.get("author_name", ["Unknown"])[0],
         "year_published": book_data.get("first_publish_year"),
         "isbn": book_data.get("isbn", [""])[0], # multiple ISBNs possible
-        "rating": None,  # Open Library does not provide ratings
-        "book_type": None,  # Not available in Open Library
-        "status": None,  # Not available in Open Library
-        "genre": ", ".join(book_data.get("subject", [])),
         "language": ", ".join(book_data.get("language", [])),
         "synopsis": book_data.get("description", ""),
-        "review": None,  # Not available in Open Library
         "cover_image": f"https://covers.openlibrary.org/b/id/{book_data.get('cover_i')}-L.jpg" if book_data.get("cover_i") else None,
         "page_count": book_data.get("number_of_pages_median",0),  # Not available in Open Library
-        "series": None,  # Not available in Open Library
-        "tags": None,  # Not available in Open Library
         "subject": ", ".join(book_data.get("subject", [])),
+        # some books have also ratings_average
+        "rating": round(book_data.get("ratings_average", 0), 1),  # Not available in Open Library
     }
 
     # also available:
