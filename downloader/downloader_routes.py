0dd6cb5dd79f84e2f301e33301843e9b6e3f3cd1
Juraj Borza <jborza@gmail.com>
2025-05-18 15:48:04 +0200

    not returning 400/404 errors if book coveer downloader has nothing to do

--- downloader/downloader_routes.py
+++ downloader/downloader_routes.py
@@ -10,8 +10,13 @@ def download_book_covers():
     # look in the database for books with a remote_image_url, fetch one
     book = Book.query.filter(Book.remote_image_url.isnot(None)).first()
     if not book:
-        return jsonify(message="No books with remote_image_url found"), 404
+        return jsonify(message="No books with remote_image_url found"), 200
     url = book.remote_image_url
+    if url == '':
+        # broken remote image url, fix it
+        book.remote_image_url = None
+        db.session.commit()
+        return jsonify(message="Book has no remote_image_url"), 200
     cover_image = download_cover_image(url)
     book.cover_image = cover_image
     book.cover_image_tiny = make_tiny_cover_image(cover_image)
