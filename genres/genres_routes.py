4c7eadac5d5391abd49daca83bf4b46db294d912
Juraj Borza <jborza@gmail.com>
2025-05-31 18:14:10 +0200

    improved genre listing to split genres

--- genres/genres_routes.py
+++ genres/genres_routes.py
@@ -4,16 +4,18 @@ from models import Book
 genres_bp = Blueprint('genres', __name__, url_prefix='/genres')
 
 @genres_bp.route('/api')
-def list_authors_api():
+def list_genres_api():
     query = Book.query.with_entities(Book.genre).distinct().order_by(Book.genre).all()
     items = [r for (r, ) in query]
     # some genres are in a json format
     # "['Religion']" or "['Fiction','Fantasy']"
     # buuut - do this when we store the book, not when we read it!!!!
     # some genres are in an array, so we need to flatten them
-    items = [item for sublist in items for item in sublist] if isinstance(items[0], list) else items
     # some genres are separated by commas, so we need to split them
-    items = [item.strip() for sublist in items for item in sublist.split(',')] if isinstance(items[0], str) else items
+    items = [item.split(',') for item in items if isinstance(item, str)]
+    # flatten the list of lists
+    items = [item for sublist in items for item in sublist]
+    items = [item.strip() for item in items if item]  
     # remove duplicates
     items = list(set(items))
     # remove empty items
