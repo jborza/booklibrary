7446698de035b4855cbd9d69dd57b5fd5f08a417
Juraj Borza <jborza@gmail.com>
2025-06-04 19:45:57 +0200

    generating author-title file name for book downloads - #22

--- files/file_routes.py
+++ files/file_routes.py
@@ -1,7 +1,7 @@
 from flask import abort, request, send_from_directory
 from flask import Blueprint, jsonify
 from files.files import BOOKS_DIR, get_supported_extensions, save_book_file
-from models import Book, db
+from models import Author, Book, db
 import os
 from werkzeug.utils import secure_filename
 
@@ -14,11 +14,20 @@ files_bp = Blueprint('files', __name__, url_prefix='/files')
 @files_bp.route('/<book_id>/<filename>')
 def serve_book_file(book_id, filename):
     # Sanitize filename to prevent directory traversal
+    book = Book.query.join(Book.author).filter(Book.id == book_id).first_or_404()
     filename = secure_filename(filename)
     book_folder = os.path.join(BOOKS_DIR, book_id)
     if not os.path.exists(os.path.join(book_folder, filename)):
         abort(404)
-    return send_from_directory(book_folder, filename, as_attachment=True)
+    # generate a new name of the file
+    extension = os.path.splitext(filename)[1]
+    newname = f"{book.author.name}-{book.title}{extension}"
+    # Sanitize the download name
+    newname = secure_filename(newname)
+    return send_from_directory(book_folder,
+                               filename,
+                               as_attachment=True,
+                               download_name=newname)
 
 @files_bp.route('/<int:book_id>/cover', methods=['POST'])
 def upload_cover_image(book_id):
@@ -37,7 +46,7 @@ def upload_cover_image(book_id):
     db.session.commit()
     return jsonify({'status': 'success', 'message': 'Cover image uploaded successfully'}), 200
 
-@files_bp.route('/<int:book_id>/file', methods=['POST'])
+@files_bp.route('/<int:book_id>/upload', methods=['POST'])
 def upload_book_file(book_id):
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
@@ -51,6 +60,8 @@ def upload_book_file(book_id):
     except ValueError as e:
         return jsonify({'error': str(e)}), 400
     book = Book.query.get_or_404(book_id)
-    book.file_path = path
+    # get just the filename from the path - book_filename.extension
+    filename = os.path.basename(path)
+    book.file_path = filename
     db.session.commit()
     return jsonify({'status': 'success', 'message': 'File uploaded successfully'}), 200
